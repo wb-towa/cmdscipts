b5b9d69ac981a9c71b4a6073bf1a879e26d2bd6c
William B <wb@towa.co>
2020-01-26 18:58:29 +0000

    some utilities

--- utils.sh
+++ utils.sh
@@ -4,4 +4,40 @@
 # Primarily a list of functions which should be included in othe scripts
 #
 
+in_array() {
+    # Determine if a value is in an array
+    # Usage Example:
+    # Array = arr=("1", "2", "3")
+    # if in_array "2" "${arr[@]}"; then...
+
+    # TODO: if there isn't something available already add 'get_from_array' sort of function
+    local seeking=$1; shift
+    local in=1
+
+    for element; do
+        if [[ $element == "$seeking" ]]; then
+            in=0
+            break
+        fi
+    done
+
+    return $in
+}
+
+check_error() {
+    # Check if previous command errored
+    # Show message then exit
+    # Usage Example:
+    # check_error "my previous function failed"
+
+    # TODO: optional carry on or exit immediately
+    # TODO: possibly pass in clean up functions?
+    status_code=$?
+
+    if [ ${status_code} -ne 0 ]; then
+        echo "status code = ${status_code} message: ${1}"
+        exit ${status_code};
+    fi
+}
+
 
